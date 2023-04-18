abc3da67e60cda6e60764ac84d53e39695efd62c
faisalBagaziy <faisalbagaziy@gmail.com>
2023-04-18 06:06:50 +0300

    imported the module and used it

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import myName from './myName';
 
 function component(){
     const element = document.createElement('div');
-    const elementName = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
     // lgnore above Lodash is now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], '');
-    elementName.innerHTML = myName('Faisal');
     return element;
 }
 
-document.body.appendChild(component())
+function nameComponent(){
+    const nameElement = document.createElement('div');
+    
+    nameElement.innerHTML = myName("Faisal");
+
+    return nameElement;
+}
+
+document.body.appendChild(component());
+document.body.appendChild(nameComponent());
